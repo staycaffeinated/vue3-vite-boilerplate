cf990a0c9244b5085206155555215bf6fca1b029
Jon Caulfield <danesub@yahoo.com>
2021-12-05 12:02:53 -0600

    chore: initial code base

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<template>
+  <h1>App Vue</h1>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 4000,
+  },
+  resolve: {
+    alias: {
+      '/@': path.resolve(__dirname, './src'),
+    },
+  },
+
+  plugins: [vue()]
+})
