c6cd4bcdb946a47cd951e4722999599f76fae47e
Jon Caulfield <danesub@yahoo.com>
2021-12-09 09:53:59 -0600

    chore: remove empty style blocks

--- src/App.vue
+++ src/App.vue
@@ -33,14 +33,3 @@ export default defineComponent({
     </component>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/BottomNavbar.vue
+++ src/components/BottomNavbar.vue
@@ -22,14 +22,8 @@ export default defineComponent({
 
 <template>
   <footer class="footer mt-auto py-3 bg-light">
-    <div class="container">
+    <div class="container text-center">
       <span class="text-muted">Place sticky footer content here.</span>
     </div>
   </footer>
 </template>
-
-<style scoped>
-.footer {
-  flex: 1 1 auto
-}
-</style>
